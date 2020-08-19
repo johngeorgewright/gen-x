4c7e82f7bb0f0be254a29b15abef22cac278e2ed
John Wright <jrite@protonmail.com>
2020-08-19 14:29:21 +1000

    refactor: remove ejs

--- src/scripts/generateGenXTypings.ts
+++ src/scripts/generateGenXTypings.ts
@@ -1,12 +1,48 @@
-import ejs from 'ejs'
 import * as path from 'path'
-import { readFileSync, writeFileSync } from 'fs'
+import { writeFileSync } from 'fs'
 import range from 'lodash.range'
+import last from 'lodash.last'
 
 const outputFile = path.resolve(__dirname, '..', 'types', 'GenX.ts')
-const templateFile = path.resolve(__dirname, '..', 'types', 'GenX.ts.ejs')
-const template = readFileSync(templateFile)
-const output = ejs.render(template.toString(), {
-  range: range(1, 101).map((num) => range(1, num + 1)),
-})
-writeFileSync(outputFile, output)
+const numRange = range(1, 101).map((num) => range(1, num + 1))
+
+writeFileSync(
+  outputFile,
+  `
+  import { Operator } from './Operator'
+
+  export default interface GenX {
+    genX<I, O1>(): (input?: I) => AsyncGenerator<O1>
+    
+    ${numRange
+      .map(
+        (nums) => `
+    genX<${Os(nums)}>(
+      operator1: () => O1,
+      ${otherAguments(nums)}
+    ): () => AsyncGenerator<O${last(nums)}>
+
+    genX<I, ${Os(nums)}>(
+      operator1: (input: I) => O1,
+      ${otherAguments(nums)}
+    ): (input: I) => AsyncGenerator<O${last(nums)}>
+    `
+      )
+      .join('')}
+  }
+`
+)
+
+function Os(nums: number[]) {
+  return nums.map((num) => `O${num}`).join(', ')
+}
+
+function otherAguments(nums: number[]) {
+  return nums
+    .slice(1)
+    .map(
+      (num) =>
+        `operator${num}: Operator<${num === 1 ? 'I' : `O${num - 1}`}, O${num}>,`
+    )
+    .join('\n')
+}
