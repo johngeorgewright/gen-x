61b9ee90ba7a1d0a9b51c67fb4a788a089371a23
John Wright <jrite@protonmail.com>
2020-10-15 16:10:14 +1100

    feat: add all missing Array.prototype functions to list
    affects: @gen-x/util

--- packages/util/src/list.ts
+++ packages/util/src/list.ts
@@ -10,6 +10,11 @@ export function create<T>(...items: T[]): List<T> {
   }
 }
 
+export function concat<T>({ items }: List<T>, ...listsRest: List<T>[]) {
+  const itemsRest = listsRest.map(({ items }) => items)
+  return create(items.concat(...itemsRest))
+}
+
 export function every<T>(
   { items }: List<T>,
   fn: (item: T, index: number) => boolean
@@ -24,6 +29,20 @@ export function filter<T>(
   return create(...items.filter(fn))
 }
 
+export function find<T>(
+  { items }: List<T>,
+  fn: (item: T, index: number) => boolean
+) {
+  return items.find(fn)
+}
+
+export function findIndex<T>(
+  { items }: List<T>,
+  fn: (item: T, index: number) => boolean
+) {
+  return items.findIndex(fn)
+}
+
 export function forEach<T>(
   { items }: List<T>,
   fn: (item: T, index: number) => void | symbol
@@ -35,8 +54,32 @@ export function forEach<T>(
   }
 }
 
+export function from<T>(arrayLike: ArrayLike<T>) {
+  return create(...Array.from(arrayLike))
+}
+
 export function get<T>({ items }: List<T>, index: number): T | undefined {
   return items[index]
+}
+
+export function includes<T>({ items }: List<T>, valueToFind: T) {
+  return items.includes(valueToFind)
+}
+
+export function indexOf<T>({ items }: List<T>, searchElement: T) {
+  return items.indexOf(searchElement)
+}
+
+export function join<T>({ items }: List<T>, separator?: string) {
+  return items.join(separator)
+}
+
+export function keys<T>({ items }: List<T>) {
+  return create(...items.keys())
+}
+
+export function lastIndexOf<T>({ items }: List<T>, searchElement: T) {
+  return items.lastIndexOf(searchElement)
 }
 
 export function map<T, X>(
@@ -44,6 +87,26 @@ export function map<T, X>(
   fn: (item: T, index: number) => X
 ) {
   return create(...items.map(fn))
+}
+
+export function pop<T>({ items }: List<T>) {
+  const array = [...items]
+  array.pop()
+  return create(...array)
+}
+
+export function push<T>({ items }: List<T>, item: T) {
+  const array = [...items]
+  array.push(item)
+  return create(...array)
+}
+
+export function reduce<T, X>(
+  { items }: List<T>,
+  initialValue: X,
+  fn: (acc: X, item: T, index: number) => X
+) {
+  return items.reduce(fn, initialValue)
 }
 
 export function reverse<T>({ items }: List<T>) {
@@ -54,10 +117,44 @@ export function reverse<T>({ items }: List<T>) {
   return create(...reversed)
 }
 
-export function some<T>(l: List<T>, fn: (item: T, index: number) => boolean) {
-  return l.items.some(fn)
+export function shift<T>({ items }: List<T>) {
+  const array = [...items]
+  return array.shift()
+}
+
+export function slice<T>({ items }: List<T>, start?: number, end?: number) {
+  return create(...items.slice(start, end))
+}
+
+export function sort<T>({ items }: List<T>, fn?: (a: T, b: T) => number) {
+  const array = [...items]
+  return create(...array.sort(fn))
+}
+
+export function splice<T>(
+  { items }: List<T>,
+  start: number,
+  deleteCount: number,
+  ...replacements: T[]
+) {
+  const array = [...items]
+  array.splice(start, deleteCount, ...replacements)
+  return create(...array)
+}
+
+export function some<T>(
+  { items }: List<T>,
+  fn: (item: T, index: number) => boolean
+) {
+  return items.some(fn)
 }
 
 export function toArray<T>({ items }: List<T>) {
   return [...items]
+}
+
+export function unshift<T>({ items }: List<T>, item: T) {
+  const array = [...items]
+  array.unshift(item)
+  return create(...array)
 }
