77f07f5dba8bb3d892dae0dfb5bbfb526643a9a0
John Wright <jrite@protonmail.com>
2020-10-27 12:41:17 +0000

    feat: silos
    affects: @gen-x/util
    
    Creating normal and forkable silos.

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,4 +1,6 @@
 export { default as filter } from './filter'
+export { default as forkableSilo } from './silo/forkable'
 export * as list from './list'
 export { default as run } from './run'
+export { default as silo } from './silo/stack'
 export { default as uniq } from './uniq'
