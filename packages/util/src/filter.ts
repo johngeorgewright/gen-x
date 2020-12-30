aa1531f9ed1fd3b311896c39996dab65a68512c8
John Wright <jrite@protonmail.com>
2020-12-30 21:46:44 +0000

    refactor: remove unused package
    BREAKING CHANGE:
    util package has been deprecated

--- packages/util/src/filter.ts
+++
@@ -1,7 +0,0 @@
-export default function filter<T>(fn: (input: T) => boolean) {
-  return function* (input: T) {
-    if (fn(input)) {
-      yield input
-    }
-  }
-}
--- packages/util/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as filter } from './filter'
-export * as list from './list'
-export { default as run } from './run'
-export { default as uniq } from './uniq'
